c4c24bec6991ea859941f04b680a8c75f915e7cd
Alan <113253976+MrDonkey08@users.noreply.github.com>
2025-10-18 11:30:07 -0600

    Update: New aliases

--- .config/profile/alias.sh
+++ .config/profile/alias.sh
@@ -7,6 +7,24 @@ alias us_date='iso_date'
 alias cdate='iso_date'
 alias mxn_date='date +"%d-%m-%Y"'
 
+# alias to get tempeature of the cpu cores
+alias core-temp='sensors | grep Core'
+
+# comands/alias to get public ip
+# alias pub-ip='curl https://ipinfo.io/ip'
+alias pub-ip='curl ifconfig.me'
+alias pub-ipv4='curl -4 ifconfig.me'
+alias pub-ipv6='curl -6 ifconfig.me'
+alias pub-ip-json='curl -4 ipinfo.io/json'
+alias pub-ipv4-json='curl -4 ipinfo.io/json'
+alias pub-ipv6-json='curl -6 ipinfo.io/json'
+
+# GUI apps aliases
+alias filen='setsid filen &> /dev/null &'
+alias session='setsid session &> /dev/null &'
+alias onlyoffice='setsid onlyoffice &> /dev/null &'
+alias standard-notes='setsid standard-notes &> /dev/null &'
+
 # grep
 alias grep='grep --color=auto'
 alias egrep='grep -E --color=auto'
