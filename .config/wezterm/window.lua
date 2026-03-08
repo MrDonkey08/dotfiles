f701db354752595fc63213ffb60f60b2f0b7101d
Alan <113253976+MrDonkey08@users.noreply.github.com>
2026-03-08 11:17:18 -0600

    Update: background image added

--- .config/wezterm/window.lua
+++ .config/wezterm/window.lua
@@ -4,6 +4,7 @@
 local module = {}
 
 function module.apply_to_config(config)
+  local dimmer = { brightness = 0.1 }
   config.window_padding = {
     left = 10,
     right = 10,
@@ -12,6 +13,20 @@ function module.apply_to_config(config)
   }
 
   config.window_background_opacity = 0.9
+
+  config.window_background_image_hsb = {
+    brightness = 0.03, -- Equivalent to tint 0.97 (inverted: 1.0 - 0.97)
+  }
+
+  config.background = {
+    {
+      source = {
+        File = "/usr/share/backgrounds/fondos/chica-samurai-con-mascara_3840x2160_xtrafondos.com.jpg",
+      },
+      hsb = { brightness = 0.3 },
+      horizontal_align = "Center",
+    },
+  }
 end
 
 return module
